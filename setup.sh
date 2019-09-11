3cea886d321add314de1e27a1c673d78ca148d0f
Janne Enberg <janne.enberg@lietu.net>
2019-09-11 16:03:46 +0300

    Cleanup & fixes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,21 @@ set -exuo pipefail
 
 bundle install
 
+# Set up Node
+nvm install $NODE_VERSION
+nvm alias default node
+
 # Prepare raml2markdown
 cd raml2markdown
 npm install
+
+# Fix running node via shebang on Linux - developers on macs are the worst
+sed -Ei 's@env node --harmony@env node@' node_modules/.bin/*
+
 cd ..
+
+# Install all the dependencies required to
+# generate and validate code examples based
+# on API specification in `raml2markdown/src`.
+bash scripts/code-examples/install-deps.sh
+
